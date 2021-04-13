282ecb31d675e4c5ed9a6a423658101a45fbe4ca
Szak0 <sattilabence@gmail.com>
2021-04-13 15:10:36 +0200

    Removed inception

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -2,9 +2,6 @@ docker run \
   --name jenkins-blueocean \
   --rm \
   --detach \
-  --env DOCKER_HOST=unix:///var/run/docker.sock \
-  --env DOCKER_CERT_PATH=/certs/client \
-  --env DOCKER_TLS_VERIFY=1 \
   --publish 8080:8080 \
   --publish 50000:50000 \
   -v jenkins-data:/var/jenkins_home \
